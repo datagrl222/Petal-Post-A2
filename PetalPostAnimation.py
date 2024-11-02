6ed3d73b2e1efff4cfbc99ac596cd43ec0b34a9e
datagrl222 <jadebaccon@icloud.com>
2024-11-03 10:21:44 +1100

    PetalPostAnimation.py

--- PetalPostAnimation.py
+++ PetalPostAnimation.py
@@ -1,5 +1,4 @@
 from visual import *
-import smtplib
 
 # Canvas parameter
 scene = display(title='PetalPost Daisy', width=800, height=800, center=(0, 0, 0))
@@ -7,7 +6,7 @@ scene = display(title='PetalPost Daisy', width=800, height=800, center=(0, 0, 0)
 # Integrate user input for message
 message = input("Enter your desired message: ")
 
-# Create a label to display the message
+# Create a label to display the message (Code for text from LearntoTech tutorial)
 text = label(pos=(0, 0.8, 0), text=message, height=40, color=color.yellow)
 
 # Create flowers centre
@@ -22,7 +21,7 @@ for i in range(6):
 # Set the maximum petal radius
 max_radius = 0.55
 
-# Animate the petals
+# Animate the petals (Inspired by Paul McWhorter's tutorial)
 while True:
     rate(50)
     for petal in petals:
